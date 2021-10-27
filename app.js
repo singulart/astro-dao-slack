a5271db7739452cc59dfebeeef0e834ef07387a5
lex <7863785+singulart@users.noreply.github.com>
2021-10-28 01:24:10 +0300

    simple proposals route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var proposalsRouter = require('./routes/proposals');
 
 var app = express();
 
@@ -15,6 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/proposals', proposalsRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
