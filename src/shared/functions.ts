0ffb9715a51a3fd7f0f1029f8cbc207f6bdfcc0a
lex <7863785+singulart@users.noreply.github.com>
2021-11-02 21:42:15 +0200

    serializing transactions work in progress - issues with Borsh.js

--- src/shared/functions.ts
+++ src/shared/functions.ts
@@ -1,4 +1,8 @@
 import logger from './Logger';
+import path from "path";
+import { KeyPair, keyStores, connect } from "near-api-js";
+
+const homedir = require("os").homedir();
 
 export const pErr = (err: Error) => {
     if (err) {
@@ -9,3 +13,29 @@ export const pErr = (err: Error) => {
 export const getRandomInt = () => {
     return Math.floor(Math.random() * 1_000_000_000_000);
 };
+
+
+export const getKeyStore = () => {
+
+    const CREDENTIALS_DIR = ".near-credentials";
+    const credentialsPath = path.join(homedir, CREDENTIALS_DIR);
+    return new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
+}
+
+export const addNewKey = async (slackUser: string): Promise<string>  => {
+
+    const keyStore = getKeyStore();
+    
+    const config = {
+        keyStore,
+        networkId: "testnet",
+        nodeUrl: "https://rpc.testnet.near.org"
+    };
+
+    // const near = await connect(config);
+    const keyPair = KeyPair.fromRandom("ed25519");
+    const publicKey = keyPair.getPublicKey().toString();    
+    await keyStore.setKey(config.networkId, publicKey, keyPair);
+
+    return publicKey;  
+}
