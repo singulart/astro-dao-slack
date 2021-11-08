1b42f1c156bf07f53bef1e05590b2446f6aa6ccc
lex <7863785+singulart@users.noreply.github.com>
2021-11-08 19:18:21 +0200

    Add/remove users UI

--- src/routes/AccountManagement.ts
+++ src/routes/AccountManagement.ts
@@ -5,36 +5,8 @@ import Axios from 'axios';
 
 export async function setup(req: Request, res: Response) {
 
-    console.log(req.headers);
-    console.log(req.body);
-
     const slackReq: ISlackRequest = req.body;
     await Axios.post(slackReq.response_url, setupInitialPrompt());
 
     return res.status(200).end();
-}
-
-
-export async function addAccount(req: Request, res: Response) {
-
-    console.log(req.headers);
-    console.log(req.body);
-
-    return res.status(201).end();
-}
-
-export async function deleteAccount(req: Request, res: Response) {
-
-    console.log(req.headers);
-    console.log(req.body);
-
-    return res.status(201).end();
-}
-
-export async function viewAccounts(req: Request, res: Response) {
-
-    console.log(req.headers);
-    console.log(req.body);
-
-    return res.status(201).end();
 }
