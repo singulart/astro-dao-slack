6162524be7376d32c7418ca7d1554cb9b3ac3585
lex <7863785+singulart@users.noreply.github.com>
2021-11-09 20:21:13 +0200

    Get DAO Balance fix

--- src/routes/Balance.ts
+++ src/routes/Balance.ts
@@ -3,6 +3,7 @@ import Axios from 'axios';
 import { ISlackRequest } from "../entities/SlackRequest";
 import { IDao } from "../entities/Proposal";
 import { formatNearAmount } from "near-api-js/lib/utils/format";
+import logger from "../shared/Logger";
 
 export const getBalance = async (req: Request, res: Response) => {
 
@@ -12,12 +13,14 @@ export const getBalance = async (req: Request, res: Response) => {
         return res.send("Usage: `/balance <DAO name>`");
     }
 
-    const daoResponseEntity = await Axios.get(`${process.env.ASTRO_API}/daos/${slackReq.text}`);
-    if(daoResponseEntity.status != 200) {
+    try {
+        const daoResponseEntity = await Axios.get(`${process.env.ASTRO_API}/daos/${slackReq.text}`);
+
+        const dao: IDao = daoResponseEntity.data;
+
+        return res.send(`\`${dao.id}\` treasury balance: *NEAR ${formatNearAmount(dao.amount, 2)}*`);
+    } catch(error) {
+        logger.err(error);
         return res.send("No info");
     }
-    
-    const dao: IDao = daoResponseEntity.data;
-
-    return res.send(`\`${dao.id}\` treasury balance: *NEAR ${formatNearAmount(dao.amount, 2)}*`);
 }
